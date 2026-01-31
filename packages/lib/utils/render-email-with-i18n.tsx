ba9e9397c6b3d1eab9e8b33f918287f3d8a61938
Ram Mohan Annamalai <rameojack@icloud.com>
2026-01-31 14:47:49 +0530

    Working and it fixed certain issues

--- packages/lib/utils/render-email-with-i18n.tsx
+++ packages/lib/utils/render-email-with-i18n.tsx
@@ -1,32 +1,40 @@
-import type { RenderOptions } from '@signtusk/email/render';
-import { renderWithI18N } from '@signtusk/email/render';
+import type { SimpleRenderOptions } from "@signtusk/email/render-simple";
+import { renderSimple } from "@signtusk/email/render-simple";
 
-import { getI18nInstance } from '../client-only/providers/i18n-server';
+import { getI18nInstance } from "../client-only/providers/i18n-server";
 import {
   APP_I18N_OPTIONS,
-  type SupportedLanguageCodes,
   isValidLanguageCode,
-} from '../constants/i18n';
+  type SupportedLanguageCodes,
+} from "../constants/i18n";
+
+// Extend SimpleRenderOptions to include branding and lang
+export type RenderEmailOptions = SimpleRenderOptions & {
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  lang?: SupportedLanguageCodes | (string & {});
+  branding?: any; // Accept branding but ignore it for now
+};
 
 export const renderEmailWithI18N = async (
   component: React.ReactElement,
-  options?: RenderOptions & {
-    // eslint-disable-next-line @typescript-eslint/ban-types
-    lang?: SupportedLanguageCodes | (string & {});
-  },
+  options?: RenderEmailOptions
 ) => {
   try {
-    const { lang: providedLang, ...otherOptions } = options ?? {};
+    const { lang: providedLang, branding, ...otherOptions } = options ?? {};
 
-    const lang = isValidLanguageCode(providedLang) ? providedLang : APP_I18N_OPTIONS.sourceLang;
+    const lang = isValidLanguageCode(providedLang)
+      ? providedLang
+      : APP_I18N_OPTIONS.sourceLang;
 
     const i18n = await getI18nInstance(lang);
 
     i18n.activate(lang);
 
-    return renderWithI18N(component, { i18n, ...otherOptions });
+    // Use renderSimple to avoid React context issues with I18nProvider in serverless environments
+    // Note: branding is ignored in simple render mode
+    return renderSimple(component, otherOptions);
   } catch (err) {
     console.error(err);
-    throw new Error('Failed to render email');
+    throw new Error("Failed to render email");
   }
 };
